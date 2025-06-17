503dfc57e8bb8204c624d6a13f6b296cec49f926
he0xwhale <he0xwhale@gmail.com>
2025-06-17 11:01:54 +0800

    feat: add friends page and SEO optimizations

--- scripts/helpers/structured-data/lib/person.js
+++ scripts/helpers/structured-data/lib/person.js
@@ -1,16 +1,16 @@
 module.exports = (hexo, option) => {
-  const { config } = hexo;
+    const { config } = hexo;
 
-  return {
-    "@context": "https://schema.org.cn",
-    "@type": "Person",
-    name: option.person.name,
-    image: {
-      "@type": "ImageObject",
-      url: option.logo.path,
-    },
-    url: option.person.url,
-    sameAs: option.person.sns,
-    description: option.person.description,
-  };
+    return {
+        "@context": "https://schema.org",
+        "@type": "Person",
+        name: option.person.name,
+        image: {
+            "@type": "ImageObject",
+            url: option.logo.path,
+        },
+        url: option.person.url,
+        sameAs: option.person.sns,
+        description: option.person.description,
+    };
 };
